a9986429f697a6601c5fd7e177d8b0d12eb693c5
Ferruccio Islam Bisceglia <ferruccio3214@gmail.com>
2023-08-01 09:40:12 +0200

    Created sample nodejs workflow

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Error starting the server:', err);
+  } else {
+    console.log(`Server is running on http://localhost:${port}`);
+  }
+});
